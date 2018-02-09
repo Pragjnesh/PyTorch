6375bd7cab75fa72f9b4533720558d0feed0be73
Pragjnesh <pchitimi@eng.ucsd.edu>
2018-02-09 12:14:04 -0800

    baby v0.1

--- baby.py
+++ baby.py
@@ -39,3 +39,24 @@ test_data = DataLoader(
     shuffle = True,
     num_workers = 4
 )
+
+
+model_conv = models.resnet18(pretrained=True)
+for param in model_conv.parameters():
+    param.requires_grad = False
+
+# Parameters of newly constructed modules have requires_grad=True by default
+num_ftrs = model_conv.fc.in_features
+model_conv.fc = nn.Linear(num_ftrs, 256)
+
+if use_gpu:
+    model_conv = model_conv.cuda()
+
+criterion = nn.CrossEntropyLoss()
+
+# Observe that only parameters of final layer are being optimized as
+# opoosed to before.
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
+
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
